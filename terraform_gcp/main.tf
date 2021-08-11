2ec0de1c8594756e6315b17bb4be9e657241f673
BOUJMAI Fahd <fahd.boujmai@soprasteria.com>
2021-08-11 14:41:16 +0200

    gcp tutorial folder

---
+++ terraform_gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/fboujmai/ok/testing-project-322609-10e6f686ec87.json")
+
+  project = "testing-project-322609"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
