eeb5881ea33dc30c0c9474c4559e8598ba908292
BOUJMAI Fahd <fahd.boujmai@soprasteria.com>
2021-08-12 13:47:27 +0200

    gcp tutorial done

--- terraform_gcp/main.tf
+++ terraform_gcp/main.tf
@@ -8,13 +8,31 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("C:/Users/fboujmai/ok/testing-project-322609-3aaa148f7f92.json")
+  credentials = file(var.credentials_file)
 
-  project = "testing-project-322609"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["web","dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
---
+++ terraform_gcp/terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "tutorial-project-322710"
+credentials_file         = "C:/Users/fboujmai/ok/tutorial-project-322710-6108de1ca292.json"
